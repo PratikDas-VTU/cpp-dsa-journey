0653977fca92474bd89a03b61224db2165ff5816
Pratik Das <vtu30493@veltech.edu.in>
2026-03-15 14:47:54 +0530

    Created first C++ hello World Program

--- 01_Basics/01_helloWorld.cpp
+++ 01_Basics/01_helloWorld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main(){
    std::cout << "Starting my C++ DSA Journey!" <<std::endl; // <<std::endl for new line
+   
    std::cout << "Date 14th March 2026" <<'\n';  //  <<'\n' for new line
     return 0;
 }
