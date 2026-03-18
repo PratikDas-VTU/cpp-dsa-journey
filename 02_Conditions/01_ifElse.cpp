07ef2d1a1379a36c19055c57e600dd8342ba2583
Pratik Das <vtu30493@veltech.edu.in>
2026-03-18 23:47:40 +0530

    add ifElse program for adult - minor classification

--- 02_Conditions/01_ifElse.cpp
+++ 02_Conditions/01_ifElse.cpp
@@ -7,7 +7,13 @@ int main () {
     std::cout << "Enter you age : " ;
     std:: cin >> age ;
 
-     
+     if(age < 0 || age > 120){
+        std::cout << "Enter a Valid age !!";
+     } else if (age >= 18){
+        std::cout << "You are an adult !!";
+     } else {
+        std::cout << "You are a minor";
+     }
 
 
 
