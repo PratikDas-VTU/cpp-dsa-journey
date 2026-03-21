0ba411ff1d6efa62e30d15d3f13906e53aa674cc
Pratik Das <vtu30493@veltech.edu.in>
2026-03-21 12:21:03 +0530

    Added example of const parameter

---
+++ 06_Pointers_and_Memory/03_constParameters.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+void printInfo(const std::string name, const int age);
+
+int main()
+{
+    std::string name = "Bro";
+    int age = 21;
+
+    printInfo(name, age);
+
+    return 0;
+}
+
+void printInfo(const std::string name, const int age)
+{
+    std::cout << name << '\n';
+    std::cout << age << '\n';
+}
