413742db7c97623f4ce7ca7874de2d3cb89fa3d8
Pratik Das <vtu30493@veltech.edu.in>
2026-04-02 22:45:40 +0530

    Implemented sequential number and character matrix patterns using nested loops in C++

--- DSA/Patterns/squarePatternContiNumbers.cpp
+++ DSA/Patterns/squarePatternContiNumbers.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main(){
 
     int n=3;
+
+    // For number
     int num = 1;
+
+
     for(int i = 0; i<= n-1; i++){
         for(int j =0;j<=n-1;j++){
             cout << num << " ";
@@ -17,6 +21,25 @@ int main(){
         1 2 3 
         4 5 6 
         7 8 9 
+        */ 
+    
+    cout << endl;
+
+    // For Character
+    char ch = 'A';
+    for(int i = 0; i<= n-1; i++){
+        
+        for(int j =0;j<=n-1;j++){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+	/*Output :
+        A B C 
+        D E F 
+        G H I 
         */ 
     return 0;
 }
