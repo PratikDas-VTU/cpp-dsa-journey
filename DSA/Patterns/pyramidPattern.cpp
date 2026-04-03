c426d90e1725bcb1b1473b9fd08274d06925cf79
Pratik Das <vtu30493@veltech.edu.in>
2026-04-03 14:07:27 +0530

    DSA Patterns: Implemented palindromic number pyramid using nested loops

---
+++ DSA/Patterns/pyramidPattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=4;
+   
+    for(int i = 0 ; i<n;i++){
+
+    
+
+        for(int j = n-i;j>0;j--){
+            cout << " ";
+        }
+        for(int j = 0;j<=i;j++){
+            cout << j+1;
+            
+        }
+        for(int j = i;j>0;j--){
+            cout << j;
+        }
+        cout<< endl;
+    }
+
+	/*Output :
+        
+        */ 
+    return 0;
+}
